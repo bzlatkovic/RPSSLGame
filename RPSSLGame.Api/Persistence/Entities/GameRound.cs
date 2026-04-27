3ba58c93d9591bfb69d2d6843a7730080fdfbcc4
bzlatkovic <banezlatkovic95@gmail.com>
2026-04-27 21:35:36 +0200

    feat: add game round entity

---
+++ RPSSLGame.Api/Persistence/Entities/GameRound.cs
@@ -0,0 +1,26 @@
+﻿using RPSSLGame.Api.Domain;
+
+namespace RPSSLGame.Api.Persistence.Entities;
+
+public class GameRound
+{
+    private GameRound() { }
+
+    public Guid Id { get; private set; }
+    public Choice PlayerChoice { get; private set; }
+    public Choice ComputerChoice { get; private set; }
+    public GameResult Result { get; private set; }
+    public DateTime PlayedAt { get; private set; }
+
+    public static GameRound Create(Choice playerChoice, Choice computerChoice, GameResult result)
+    {
+        return new GameRound
+        {
+            Id = Guid.CreateVersion7(),
+            PlayerChoice = playerChoice,
+            ComputerChoice = computerChoice,
+            Result = result,
+            PlayedAt = DateTime.UtcNow
+        };
+    }
+}
