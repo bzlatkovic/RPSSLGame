ed1dab07f98632f17373f683a46f55a9af57091f
bzlatkovic <banezlatkovic95@gmail.com>
2026-04-27 21:36:02 +0200

    feat: add game rount entity configuration

---
+++ RPSSLGame.Api/Persistence/Configurations/GameRoundConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RPSSLGame.Api.Persistence.Entities;
+
+namespace RPSSLGame.Api.Persistence.Configurations;
+
+public class GameRoundConfiguration : IEntityTypeConfiguration<GameRound>
+{
+    public void Configure(EntityTypeBuilder<GameRound> builder)
+    {
+        builder.ToTable("game_rounds");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnType("uuid")
+            .ValueGeneratedNever()
+            .HasColumnName("id");
+
+        builder.Property(x => x.PlayerChoice)
+            .IsRequired()
+            .HasConversion<int>()
+            .HasColumnName("player_choice");
+
+        builder.Property(x => x.ComputerChoice)
+            .IsRequired()
+            .HasConversion<int>()
+            .HasColumnName("computer_choice");
+
+        builder.Property(x => x.Result)
+            .IsRequired()
+            .HasConversion<int>()
+            .HasColumnName("result");
+
+        builder.Property(x => x.PlayedAt)
+            .IsRequired()
+            .HasColumnName("played_at")
+            .HasColumnType("timestamp with time zone");
+    }
+}
