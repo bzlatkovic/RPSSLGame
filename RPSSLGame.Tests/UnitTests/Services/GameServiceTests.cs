6020b64d40dc5a6918ce1faeaf36063063219ad3
bzlatkovic <banezlatkovic95@gmail.com>
2026-04-28 20:05:16 +0200

    tests: add unit tests for stats method in game service

--- RPSSLGame.Tests/UnitTests/Services/GameServiceTests.cs
+++ RPSSLGame.Tests/UnitTests/Services/GameServiceTests.cs
@@ -1,8 +1,10 @@
 ﻿using FluentAssertions;
+using Microsoft.Extensions.Logging;
 using NSubstitute;
 using RPSSLGame.Api.Domain;
 using RPSSLGame.Api.Models;
 using RPSSLGame.Api.Persistence.Entities;
+using RPSSLGame.Api.Persistence.ReadModels;
 using RPSSLGame.Api.Persistence.Repositories;
 using RPSSLGame.Api.Services;
 
@@ -12,13 +14,15 @@ public class GameServiceTests
 {
     private readonly IGameRoundRepository _gameRoundRepository;
     private readonly GameService _gameService;
+    private readonly ILogger<GameService> _logger;
     private readonly IRandomNumberService _randomNumberService;
 
     public GameServiceTests()
     {
         _randomNumberService = Substitute.For<IRandomNumberService>();
         _gameRoundRepository = Substitute.For<IGameRoundRepository>();
-        _gameService = new GameService(_randomNumberService, _gameRoundRepository);
+        _logger = Substitute.For<ILogger<GameService>>();
+        _gameService = new GameService(_randomNumberService, _gameRoundRepository, _logger);
     }
 
     [Fact]
@@ -83,5 +87,51 @@ public class GameServiceTests
                 gr.ComputerChoice == Choice.Paper &&
                 gr.Result == GameResult.Tie),
             Arg.Any<CancellationToken>());
+    }
+    
+    [Fact]
+    public async Task GetStatsAsync_ShouldReturnEmptyResponse_WhenNoStatsExist()
+    {
+        _gameRoundRepository.GetStatsAsync(Arg.Any<CancellationToken>()) .Returns([]);
+        
+        var result = await _gameService.GetStatsAsync(Arg.Any<CancellationToken>());
+        
+        result.Should().NotBeNull();
+        result.TotalRounds.Should().Be(0);
+        result.ChoiceStats.Should().BeNullOrEmpty();
+    }
+    
+    [Fact]
+    public async Task GetStatsAsync_ShouldCalculateAggregatesCorrectly()
+    {
+        // Arrange
+        var stats = new List<ChoiceStats>
+        {
+            new() { Choice = Choice.Rock, TimesPlayed = 10, Wins = 5, Losses = 3, Ties = 2 },
+            new() { Choice = Choice.Paper, TimesPlayed = 20, Wins = 2, Losses = 15, Ties = 3 }
+        };
+
+        _gameRoundRepository.GetStatsAsync(Arg.Any<CancellationToken>()).Returns(stats);
+
+        // Act
+        var result = await _gameService.GetStatsAsync(CancellationToken.None);
+        result.TotalRounds.Should().Be(30); 
+        result.TotalWins.Should().Be(7);    
+        result.TotalLosses.Should().Be(18); 
+        result.TotalTies.Should().Be(5);    
+        result.MostPlayedChoice.Should().BeEquivalentTo(new ChoiceDto(Choice.Paper)); 
+        result.MostWinningChoice.Should().BeEquivalentTo(new ChoiceDto(Choice.Rock));
+        result.ChoiceStats.Should().NotBeNullOrEmpty();
+        result.ChoiceStats.Should().HaveCount(2);
+        var rock = result.ChoiceStats.Should().Contain(x=> x.Choice.Id == (int)Choice.Rock).Which;
+        rock.TimesPlayed.Should().Be(10);
+        rock.Wins.Should().Be(5);
+        rock.Losses.Should().Be(3);
+        rock.Ties.Should().Be(2);
+        var paper = result.ChoiceStats.Should().Contain(x=> x.Choice.Id == (int)Choice.Paper).Which;
+        paper.TimesPlayed.Should().Be(20);
+        paper.Wins.Should().Be(2);
+        paper.Losses.Should().Be(15);
+        paper.Ties.Should().Be(3);
     }
 }
